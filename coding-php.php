d4e7fed61ebfb6cf34d2974e033fa9df7daf0421
desi456 <59003474+desi456@users.noreply.github.com>
2019-12-19 10:32:32 +0700

    membuat coding nama.php

---
+++ coding-php.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> MENAMPILKAN NAMA TIDAK MENGGUNAKAN KEY </title>
+</head>
+<body>
+<?php
+$nama = array(
+"Andri",
+"Joko",
+echo $nama[0];
+echo"<br>";
+echo$nama[1];
+echo"<br>";
+?>
+</body>
+</html>
